3a7fe72e839e6129f1c74fcd52cb0d6c5703347a
warriorcris25 <104987643+warriorcris25@users.noreply.github.com>
2023-12-25 18:08:18 -0500

    Conexion
    Clase conexion de la base de datos

---
+++ Sol_PuntoVenta/Sol_PuntoVenta.Datos/Conexion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Sol_PuntoVenta.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+
+        private Conexion() 
+        {
+            this.Base = "BD_PUNTOVENTA";
+            this.Servidor = "EDQE-PHP-012FC\\SQLEXPRESS";
+            this.Usuario = "user_sistema";
+            this.Clave = "tigre#78912";
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor +
+                                       "; Database=" + this.Base +
+                                       "; User Id=" + this.Usuario +
+                                       "; Password=" + this.Clave;
+            }
+            catch (Exception ex)
+            {
+
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+        public static Conexion getInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
