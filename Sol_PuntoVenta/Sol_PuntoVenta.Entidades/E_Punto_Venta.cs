b808c773e5185acd53a6d1d07504f656f87868fe
warriorcris25 <104987643+warriorcris25@users.noreply.github.com>
2023-12-25 18:19:20 -0500

    Get

---
+++ Sol_PuntoVenta/Sol_PuntoVenta.Entidades/E_Punto_Venta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_PuntoVenta.Entidades
+{
+    public class E_Punto_Venta
+    {
+        public int Codigo_pv { get; set; }
+        public string Descripcion_pv { get; set; }
+
+    }
+}
