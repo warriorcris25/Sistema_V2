5d75369f1d536530d86854ec3e8616bc0e9814f4
warriorcris25 <104987643+warriorcris25@users.noreply.github.com>
2023-12-25 18:43:21 -0500

    Negocio
    Parte negocio del punto de venta acceso a la capa de lo que se genera

---
+++ Sol_PuntoVenta/Sol_PuntoVenta.Negocio/N_Punto_Venta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Sol_PuntoVenta.Entidades;
+using Sol_PuntoVenta.Datos;
+
+namespace Sol_PuntoVenta.Negocio
+{
+    public class N_Punto_Venta
+    {
+        public static DataTable Listado_pv(string cTexto)
+        {
+            D_Punto_Venta Datos = new D_Punto_Venta();
+            return Datos.Listado_pv(cTexto);
+        }
+        public static string Guardar_pv(int nOpcion, E_Punto_Venta oPropiedad)
+        {
+            D_Punto_Venta Datos = new D_Punto_Venta();
+            return Datos.Guardar_pv(nOpcion, oPropiedad);
+        }
+        public static string Eliminar_pv(int nCodigo)
+        {
+            D_Punto_Venta Datos = new D_Punto_Venta();
+            return Datos.Eliminar_pv(nCodigo);
+        }
+    }
+}
